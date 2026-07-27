b9617aabbe700900b55203d61aa1a1c0803e3402
Shain Anderson Alir Chino Calle <schinoc@unjbg.edu.pe>
2026-07-27 16:13:09 -0500

    Actualiza ejercicio menor de dos numeros y el mayor

--- Entero Mayor.cpp
+++ Entero Mayor.cpp
@@ -1,21 +1,35 @@
 #include <iostream>
 using namespace std;
-int main ()
+
+int main()
 {
-	int a, b;
-	
-	cout<<"Ingrese el primer numero entero: ";
-	cin>>a;
-	cout<<"Ingrese el segundo numero entero: ";
-	cin>>b;
-	
-	if (a==b){
-		cout<<"Ambos numeros son iguales";
-	} else if (a<=b){
-		cout<<"El menor numero es: "<<a;
-	} else {
-		cout<<"El menor numero es: "<<b;
-	}
-	
-	return 0;
+    int a, b;
+
+    cout << "=== MENOR DE DOS NUMEROS ===" << endl;
+
+    cout << "Ingrese el primer numero entero: ";
+    cin >> a;
+
+    cout << "Ingrese el segundo numero entero: ";
+    cin >> b;
+
+    if (a == b)
+    {
+        cout << "Ambos numeros son iguales.";
+    }
+    else
+    {
+        if (a < b)
+        {
+            cout << "El numero menor es: " << a << endl;
+            cout << "El numero mayor es: " << b << endl;
+        }
+        else
+        {
+            cout << "El numero menor es: " << b << endl;
+            cout << "El numero mayor es: " << a << endl;
+        }
+    }
+
+    return 0;
 }
